d2329f9604d4bc5d80a06508a3afcd25a2b8d4fe
GiordanoDiego <diegomgiordano96@gmail.com>
2024-01-10 17:38:06 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            newTodo: '',
             todos: [
                 {
                     text: 'mangiare',
@@ -35,6 +36,16 @@ createApp({
         removeTodo(index){
             this.todos.splice(index, 1); //primo argomento indice da cui inizio la cancellazione, secondo argomento indici da cancellare
         },
+        addTodo(){
+            if(this.newTodo.trim().length>0){
+                const newTodo = {
+                    text: this.newTodo.trim(),
+                    done: false
+                };
+                this.todos.push(newTodo);
+                this.newTodo =''; //svuoto il campo di aggiunta
+            }
+        }
         
     }
 }).mount('#app');
